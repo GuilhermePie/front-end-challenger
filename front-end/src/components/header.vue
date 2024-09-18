5a89320d9d88191e07c7d4b3c3c2e0dec1274cdc
GuilhermePie <guilhermeSpiedade@gmail.com>
2024-09-18 16:05:26 -0300

    controle de estado

--- front-end/src/components/header.vue
+++ front-end/src/components/header.vue
@@ -1,13 +1,22 @@
 <template>
     <header class="navbar">
         <img src="../assets/images/logo-barato-coletivo-rem.png" alt="Logo do barato coletivo" class="img" @click="voltar()">
-        <button class="btn" @click="signIn()">Login</button>
-        <!-- <font-awesome-icon :icon="['fas', 'user']" class="user-icon"/> -->
+        <button class="btn" @click="signIn()" v-if="!token_exist">Login</button>
+        <div class="user-box" v-if="token_exist">
+            <button class="btn" @click="logOut()" >Log Out</button>  
+            <font-awesome-icon :icon="['fas', 'user']" class="user-icon"/>
+        </div>
     </header>
 </template>
 
 <script>
     export default {
+        data(){
+            return{
+                token_exist: localStorage.getItem('token')
+            }
+        },
+
         methods:{
             voltar(){
                 this.$router.push({ path: '/' })
@@ -15,19 +24,35 @@
 
             signIn(){
                 this.$router.push({ path: '/tela_login' })
+            },
+
+            logOut(){
+                this.$router.push({ path: '/tela_login' })
+                const token = localStorage.removeItem('token')
             }
         }
     }
 </script>
 
 <style scoped>
+    .user-box{
+        display: flex;
+        gap: 10px;
+    }
+
     .btn{
+        font-weight: bold;
         border: 2px solid white;
         border-radius: 20px;
         padding: 10px 25px;
         background-color: rgba(0, 0, 0, 0);
         color: white;
         cursor: pointer;
+    }
+
+    .btn:hover{
+        background-color: white;
+        color:black;
     }
 
     .img{
@@ -49,6 +74,11 @@
         border-radius: 50%;
         padding: 10px 11px;
         cursor: pointer;
+    }
+
+    .user-icon:hover{
+        background-color: white;
+        color: black;
     }
 
     @media only screen and (max-width: 600px) {
